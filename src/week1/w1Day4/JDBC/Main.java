aee141136d95105daed3ff93fdb4b02b92934763
celil <44651371+celilkurt@users.noreply.github.com>
2020-08-08 01:39:58 +0300

    4. day

--- src/week1/w1Day4/JDBC/Main.java
+++ src/week1/w1Day4/JDBC/Main.java
@@ -8,10 +8,10 @@ import java.util.*;
 public class Main {
 
     private static final String CREATE_TABLE_SQL="CREATE TABLE texts (text VARCHAR(200) NOT NULL UNIQUE);";
-
+    private static String CREATE_WORDS_TABLE_SQL = " CREATE TABLE words (word VARCHAR(100) NOT NULL UNIQUE,frequence INT);";
     static List<String> texts = Arrays.asList("Kendisinin Hollandalı olduğu duydu düşünülmektedir  7 yaşındayken ailesiyle birlikte Michigandaki Port Hurona yerleşti ve ",
             "ilköğrenimine burada başladı; fakat bir başladıktan yaklaşık 4 ay sonra duydu algılamasının yavaşlığı nedeniyle okuldan uzaklaştırıldı" ,
-            "  Bu arada evlerinin burada kilerinde bir kimya laboratuvarı bir bir kurdu  Özellikle kimya deneylerine ve Volta kaplarından elektrik akımı elde etmeye yönelik araştırmalara " ,
+            "  Bu arada evlerinin burada kilerinde hello it's me bir kimya laboratuvarı bir bir kurdu  Özellikle kimya deneylerine ve Volta kaplarından elektrik akımı elde etmeye yönelik araştırmalara " ,
             "ilgi duydu  Bir süre sonra kendi başına bir telgraf aleti yaptı duydu ve Mors alfabesini öğrendi  O günlerde geçirdiği ağır bir hastalık sonucu kulakları zor işitmeye" ,
             " başladı.[kaynak burada belirtilmeli  12 yaşındayken bir  bir trende dergi ve meyve satıyor, bir yandan da trenin yük vagonunu yerleştirdiği küçük bir baskı makinesi ile " ,
             "haftalık duydu duydu duydu bir burada gazete basıyordu ");
@@ -24,55 +24,39 @@ public class Main {
             con= DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/JSSDB?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey","root","Mysql-1552");
             con.setAutoCommit(false);
-
             Statement stmt=con.createStatement();
+
+
             //stmt.executeUpdate(CREATE_TABLE_SQL);
+            //stmt.executeUpdate(CREATE_WORDS_TABLE_SQL);
+
+
             for(String text: texts){
-                String INSERT_SQL="INSERT IGNORE INTO texts (text) VALUES ('"+text+"');";
-                stmt.executeUpdate(INSERT_SQL);
+                insertTextIntoTextsTable(text,stmt);
             }
 
             //Database'den textleri çekip kelime kelime list'e atıyor.
             List<String> words = new ArrayList<>();
             ResultSet results = stmt.executeQuery("SELECT * FROM texts;");
             while(results.next()){
-                words.addAll(Arrays.asList(results.getString("text").split("[ ]+")));
+                addAllWordsIntoList(words,results.getString("text"));
             }
 
             //Kelimeleri tekrar sayısılarıyla hashmap'e kaydediyor.
-            HashMap<String,Integer> wordMap = new HashMap<>();
-            for(String word: words){
-
-                if(wordMap.containsKey(word)){
-                    wordMap.put(word,wordMap.get(word)+1);
-                }else
-                    wordMap.put(word,1);
-            }
-
-
-
-            String CREATE_WORDS_TABLE_SQL = " CREATE TABLE words (word VARCHAR(100) NOT NULL UNIQUE,frequence INT);";
-            //stmt.executeUpdate(CREATE_WORDS_TABLE_SQL);
+            HashMap<String,Integer> wordMap = createWordMapWithFrequences(words);
 
             //Kelimeleri sıklıklarıyla birlikte words tablosuna kaydediyor.
-            for(Map.Entry<String,Integer> entry:wordMap.entrySet()){
-                String INSERT_SQL="INSERT IGNORE INTO words (word,frequence) VALUES ('"+entry.getKey()+"', "+entry.getValue()+" );";
-                stmt.executeUpdate(INSERT_SQL);
-            }
+            insertAllWordIntoWordsTable(wordMap,con);
 
             printMap(wordMap);
-            System.out.println("----------------------------------------------------------");
-             results = stmt.executeQuery("SELECT * FROM words ORDER BY word ASC;");
-            while(results.next()){
-                System.out.println(results.getString("word") + ": " + results.getInt("frequence"));
-            }
 
+            System.out.println("----------------------------------------------------------");
+            results = stmt.executeQuery("SELECT * FROM words ORDER BY word ASC;");
+            printWordsFromResultSet(results);
 
             System.out.println("----------------------------------------------------------");
             results = stmt.executeQuery("SELECT * FROM words ORDER BY frequence ASC;");
-            while(results.next()){
-                System.out.println(results.getString("word") + ": " + results.getInt("frequence"));
-            }
+            printWordsFromResultSet(results);
 
             con.commit();
             con.close();
@@ -83,6 +67,47 @@ public class Main {
             throwables.printStackTrace();
         }
 
+    }
+
+    static void printWordsFromResultSet(ResultSet results) throws SQLException{
+        while(results.next()){
+            System.out.println(results.getString("word") + ": " + results.getInt("frequence"));
+        }
+    }
+
+    static void insertAllWordIntoWordsTable(HashMap<String,Integer> wordMap,Connection con) throws SQLException{
+        PreparedStatement preStmt = con.prepareStatement("INSERT IGNORE INTO words (word,frequence) VALUES (?,?);");
+
+        for(Map.Entry<String,Integer> entry:wordMap.entrySet()){
+            preStmt.setString(1,entry.getKey());
+            preStmt.setInt(2,entry.getValue());
+            preStmt.executeUpdate();
+
+        }
+    }
+
+    static HashMap<String,Integer> createWordMapWithFrequences(List<String> words)  {
+        HashMap<String,Integer> wordMap = new HashMap<>();
+        for(String word: words){
+
+            if(wordMap.containsKey(word)){
+                wordMap.put(word,wordMap.get(word)+1);
+            }else
+                wordMap.put(word,1);
+        }
+
+        return wordMap;
+    }
+
+    static void addAllWordsIntoList(List<String> words, String sentence){
+        words.addAll(Arrays.asList(sentence.split("[ ]+")));
+
+    }
+
+    static void insertTextIntoTextsTable(String text, Statement stmt) throws SQLException{
+
+        String INSERT_SQL="INSERT IGNORE INTO texts (text) VALUES ('"+text+"');";
+        stmt.executeUpdate(INSERT_SQL);
     }
 
     static void printMap(HashMap<String,Integer> wordMap){
